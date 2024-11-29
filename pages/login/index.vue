b23e1f1e4d6eab08d6a2fc3a6dfcf9435273f1f2
김진욱 <jwook0318@gmail.com>
2024-11-29 16:30:12 +0900

    feat(config): supabase config

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+const supabase = useSupabaseClient()
 
+const signInWithOAuth = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm',
+    },
+  })
+  if (error)
+    console.log(error)
+}
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error)
+    console.log(error)
+}
 </script>
 
 <template>
   <div>
     <h1>Login</h1>
+    <div class="flex gap-4">
+      <button type="button" class="btn__full--primary-lg" @click="signInWithOAuth">
+        구글 계정으로 로그인
+      </button>
+      <button type="button" class="btn__full--secondary-lg" @click="signOut">
+        로그아웃
+      </button>
+    </div>
   </div>
 </template>
 
