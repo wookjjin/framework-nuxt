07eafa29df6577eb50b9fe221218abc83fc22ede
JinWook <jwook0318@gmail.com>
2025-11-05 16:38:59 +0900

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
+    preset: 'netlify',
     prerender: {
       routes: [
         '/',
